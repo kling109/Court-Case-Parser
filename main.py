11911cdef6f10fb810f1dd03949c1eaaeb4f19b6
raymo116 <raymo116@mail.chapman.edu>
2020-01-09 16:41:04 -0800

    trying to get selenium to work

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 # Test main file
 
-print("Hello World!")
+# imports
+from selenium import webdriver
+
+
+# function that get's the html from a web url
+def getSource(strURL):
+    browser = webdriver.Chrome()
+    browser.get(strURL)
+    return browser.page_source
+
+print(getSource("http://google.com"))
